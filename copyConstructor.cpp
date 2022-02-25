e000c851540ab53e403d059256ad899bacd093f3
Zakariya khan <zak786khan@gmail.com>
2022-02-25 23:43:50 +0530

    L-42

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 class   Hero
@@ -10,9 +11,11 @@ class   Hero
     
     public:
     char level;
+    char *name;
 
     Hero (){
         cout<<"Default Constructor called"<<endl;
+        name  = new char[100];
     } 
 
     //Parameterised Constructors
@@ -28,14 +31,20 @@ class   Hero
     }
 
     Hero(Hero& copy ){  // pass by reference
+        char *ch = new char[strlen(copy.name)]+1;
+        strcpy(ch,copy.name);
+
         cout<<"Copy constructor called:"<<endl;
         this->health = copy.health;
         this->level  = copy.level;
     }
 
     void print(){
-        cout<<"Health: "<<this->health<<endl;
-        cout<<"Level: "<<this->level<<endl;
+        
+        cout<<endl;
+        cout<<"[Name: "<<this->name<<" ,";
+        cout<<"Health: "<<this->health<<" ,";
+        cout<<"Level: "<<this->level<<"]"<<endl<<endl;
     }
      
     int getHealth(){
@@ -46,26 +55,47 @@ class   Hero
         return level;
     }
 
-    void setHealth(int h,char name){
-        if (name  =='A')
-        {
+    void setHealth(int h){
+        // if (name  =='A')
+        // {
             health = h; 
-        }
+        // }
     }
 
     void setLevel(char ch){
         level = ch;
     }
 
+    void setName(char name[]){
+        strcpy(this->name,name);
+    }
+
 };
 
 int main(){
 
-      Hero thomas(24,'A');
-      thomas.print(); 
-      Hero shelby(thomas);  
-      shelby.print();  
+    //   Hero thomas(24,'A');
+    //   thomas.print(); 
+    //   Hero shelby(thomas);  
+    //   shelby.print();  
      
+     Hero h1;
+     h1.setHealth(15);
+     h1.setLevel('D');
+
+    char name[9] = "Zakariya";
+    h1.setName(name);
+    h1.print();
+
+    //use default constructor
+    Hero h2(h1);
+    // Hero h2 = h1;
+    h2.print();
+    h1.name[0] = 'z';
+    h1.print();
+
+    h2.print();    
+
 
 
   return 0;  

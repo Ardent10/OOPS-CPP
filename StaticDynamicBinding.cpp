1bd11bc6100998d8c4ffd15e384336afe6dafb43
Zakariya khan <zak786khan@gmail.com>
2022-04-06 20:48:45 +0530

    exp-7 & 8

---
+++ StaticDynamicBinding.cpp
@@ -0,0 +1,65 @@
+// Static Binding
+// The program decides which function to run at compile time.
+// #include<iostream>
+// using namespace std;
+
+// int sum(float a, float b){
+//     return a+b;
+// }
+
+// int sum(float a, float b,float c){
+//     return a+b+c;
+// }
+
+
+// int main()
+// {
+
+//         cout<<"Sum : "<<sum(5,12)<<endl;
+//         cout<<"Sum : "<<sum(5,1,2)<<endl;
+
+// return 0;
+
+
+
+
+// Dynamic binding:
+#include<bits/stdc++.h>
+using namespace std;
+
+class User{
+    public:
+    virtual void getPermission(){
+        cout<<"Users can see limited info "<<endl;
+    }
+};
+
+class SuperUser:public User{
+    
+    void getPermission(){
+        cout<<"Super users can see All info "<<endl;
+        
+    }
+
+};
+
+
+int main()
+{
+    User u;
+    SuperUser s;
+    list<User*>users;
+
+    users.push_back(&u);
+    users.push_back(&s);
+
+    for (User* usrPtr:users)
+    {
+        usrPtr->getPermission();
+    }
+    
+
+        
+
+return 0;
+}

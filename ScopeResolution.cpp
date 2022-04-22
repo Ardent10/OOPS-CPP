9807d61ddbfc834caff312c8d7c77d536e790462
Zakariya khan <zak786khan@gmail.com>
2022-04-22 23:01:50 +0530

    some changes

--- ScopeResolution.cpp
+++ ScopeResolution.cpp
@@ -3,12 +3,12 @@
 //2) C++ program to show that we can access a global variable
 // using scope resolution operator :: when there is a local
 // variable with same name
-
+ 
 #include<iostream>
 using namespace std;
 
 int x; // Global x
-
+  
 int main()
 {
 int x = 10; // Local x

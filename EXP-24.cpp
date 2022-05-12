70a91811b7f43301adb404e418d5ce730aea6169
Zakariya khan <zak786khan@gmail.com>
2022-05-13 00:47:02 +0530

    new exp

--- EXP-24.cpp
+++ EXP-24.cpp
@@ -1,13 +1,43 @@
 #include<iostream>
 using namespace std;
 
+template <class T>
+class Greater{
+    T a,b;
+    public:
+
+    Greater(T x, T y){
+        cout<<"Constructor is invoked "<<endl;
+        cout<<"and values of x and y are set."<<endl;
+        a = x; b = y;
+    }
+
+    void greater(){
+        if (a>b)
+        {
+            cout<<"A: "<<a<<endl;
+            cout<<"B: "<<b<<endl;
+            cout<<"A is Greater than B"<<endl;
+        }
+        else 
+        {
+            cout<<"A: "<<a<<endl;
+            cout<<"B: "<<b<<endl;
+            cout<<"B is Greater than A"<<endl;
+            
+        }
+    }
+
+    ~Greater(){
+        cout<<"Destructor is invoked";
+    }
+
+};
+
 int main()
 {
-
-    
-
-
-
+        Greater<int> g(55,60);
+        g.greater();
 
 return 0;
 }

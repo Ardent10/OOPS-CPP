0c39b7d2c69b86668e41dfcd57193d40b6a11065
Zakariya khan <zak786khan@gmail.com>
2022-05-12 23:38:24 +0530

    new

---
+++ EXP-23.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+template<class t1,class t2>
+void sum(t1 a,t2 b) 
+{
+    cout<<"\nSum: "<<a+b<<endl;
+}
+
+int main()
+{
+    int a,b;
+    float x,y;
+    cout<<"\nEnter two integer number: ";
+    cin>>a>>b;
+    cout<<"Enter two float number: ";
+    cin>>x>>y;
+    sum(a,b); 
+    sum(x,y); 
+    sum(a,x); 
+
+    return 0;
+}
